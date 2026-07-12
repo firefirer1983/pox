916aa4ab28bba99912da88a6b669264f6187ca3d
firefirer1983 <fyman.zhang@gmail.com>
2026-07-12 10:38:19 +0800

    [refact]

--- pox/callables.py
+++ pox/callables.py
@@ -1,11 +1,7 @@
-from typing import Any, cast
-from abc import abstractmethod
-from .statement import Statement, Stmt
-from .expression import Expr
-from .environment import Environment
-from .base import Visitor, LiteralTypes, PoxCallable
+from .statement import Stmt
+from .base import PoxCallable
 
-        
+
 class PoxFunction(PoxCallable):
     def __init__(self, stmt: Stmt.Function):
         self.stmt = stmt
@@ -13,10 +9,14 @@ class PoxFunction(PoxCallable):
     def arity(self):
         return len(self.stmt.parameters)
 
+    def to_str(self) -> str:
+        parameters = ",".join(self.stmt.parameters) or ""
+        return f"fun<{self.stmt.name}({parameters})>"
+
     @property
     def block(self) -> Stmt.Block:
         return self.stmt.block
 
     @property
-    def parameters(self)-> list[str]:
+    def parameters(self) -> list[str]:
         return self.stmt.parameters
